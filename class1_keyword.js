95d4ad8a39a823219eddfbdf09641de3e8853fbd
saurabh kumar <saurabhkumar9493@gmail.com>
2024-09-11 12:10:11 +0530

    first commit

---
+++ class1_keyword.js
@@ -0,0 +1,94 @@
+
+
+// console.log("hello how are you")
+// console.log(123)
+// console.log("10+10")
+// console.log(10*20)
+
+// ''
+// ""
+
+
+// console.log(123);
+// console.log("hello");
+// console.log("raman");
+// //console.log('aman');
+
+
+    //  a = 10;
+    //  a = 20;
+    //  b = 5;
+    //  b = 20;
+    //  console.log(a+b);
+   
+
+
+    //    var a = 10;
+    // var b = 20;
+    // console.log(a+b)
+
+
+    // var mo2mo1 = 10;
+    // console.log(mo2mo1);
+
+
+    // var a = 10;
+    // console.log(a);
+
+
+    //   var a = 10;
+    //   var a = 20;
+    //       console.log(a);
+
+
+    //    let a = 10;
+    //        a = 20;
+    //        a = 30;
+    //    console.log(a);
+
+
+    // const a = 10;
+    // console.log(a);
+
+
+
+    // {} block scope
+
+
+    // var a = 10;
+    //   let a = 10;
+    //    const a = 10;
+
+    //   {
+    //     console.log(a);
+    //   }
+
+
+
+    // {
+    //     // var a = 10;
+    //     // let a = 10;
+    //       const a = 10;
+    //       console.log(a)
+
+    // }
+
+  
+
+
+
+    // var a = 10;
+    // let a = 10;
+    // const a = 10;
+
+    {   
+        //  let a = 30; 
+        //     a = 40;  
+        // // console.log(a)
+        
+        const a = 20;
+            //    a = 30;
+        console.log(a)
+    }
+
+    //  console.log(a)
