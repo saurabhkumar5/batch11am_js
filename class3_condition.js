6ec14f72037ccbf0a87362aa06b838d0f3968a57
saurabh kumar <saurabhkumar9493@gmail.com>
2024-09-24 12:07:12 +0530

    math

--- class3_condition.js
+++ class3_condition.js
@@ -2,7 +2,7 @@
 // 2. if -else
 // 3. else if
 // 4. nested if-else
-// 5. switch statement
+// 5. switch statement //assignment
 
 
 
@@ -46,3 +46,96 @@
         // else{
         //     console.log("you are underage")
         // }
+
+
+
+        //  if(10>6){
+        //       if(20>21){
+        //          console.log("hello")
+        //       }
+        //       else{
+        //          console.log("world")
+        //       }
+        //  }
+        //  else{
+        //      if(10>8.6){
+        //         console.log("kullu")
+        //      }else{
+        //         console.log("byee")
+        //      }
+        //  }
+
+
+
+
+//         if(10>60){
+//                 console.log("hello")
+//         }
+//         else if(15>30){
+//                 console.log("kullu")
+//         }
+//        else if(12>100){
+//                 console.log("manali")
+//         }
+//         else{
+//                 console.log("byee")
+//         }
+
+
+//    90+ = A+
+//    85-90 = A
+//    80-85 = B+
+//    75-80 = B
+//    <75 = fail
+
+
+// let marks=86;
+
+// if(marks>=90)
+// {
+//     console.log("your grade is:A+");
+// }
+//  if(marks>=85 && marks<90)
+// {
+//     console.log("your grade is: A");
+// }
+//  if(marks>=80 && marks<85)
+// {
+//     console.log("your grade is: B+");
+// }
+//  if(marks>=75 && marks<80)
+// {
+//     console.log("your grade is: B");
+// }
+// else
+// {
+//     console.log("you have failed the exam !!");
+// }
+
+
+
+// let marks = 45;
+
+// if(marks>=90){
+//         console.log("A+")
+// }
+// else if(marks>=85){
+//         console.log("A")
+// }
+// else if(marks>=80){
+//        console.log("B+")
+// }
+// else{
+//         console.log("fail")
+// }
+
+
+
+// const username = "Aman";
+
+//     if(username=="aman"){
+//         console.log("login successfully")
+//     }
+//     else{
+//         console.log("something error")
+//     }
---
+++ class4_math.js
@@ -0,0 +1,17 @@
+// 1.Math.abs()
+// 2.Math.floor()
+// 3.Math.ceil()
+// 4.Math.pow()
+// 5.Math.random()
+
+// console.log(Math.abs(-10))
+
+// console.log(Math.floor(5.99999))
+// console.log(Math.ceil(5.2))
+ 
+// console.log(Math.pow(4,4))
+
+// console.log(Math.floor(Math.random()*100))
+
+
+
