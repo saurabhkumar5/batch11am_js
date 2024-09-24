12ad407cab1a6ed75030ab69fce87dd72b82bc26
saurabh kumar <saurabhkumar9493@gmail.com>
2024-09-24 11:07:31 +0530

    datatype

---
+++ class2_dataType.js
@@ -0,0 +1,97 @@
+// Data types
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. BigInt
+// 7. object
+// 8. Array
+// 9. Symbols
+
+
+// 1.
+//  let a = 10;
+//  console.log(typeof(a))
+
+// ''
+// ""
+// let a = "sohan";
+// let b = '123'
+// console.log(typeof(b))
+
+// let a = false; //boolean
+// console.log(typeof(a))
+
+
+// let a = 10;
+//     a = a + 20;
+//  let  b = a-5;
+//     console.log(b);
+
+
+// let a = 10;
+//     // a = a+1;
+//     //   a++; // a = a+1
+//     //   a = a+2
+//     //    a+=2 // a = a+2
+//     //    a*=4; //a = a*4
+//     // a++; post increment
+//     //    a--; post decremnt
+//     console.log(a)
+
+
+
+
+// let a = 10;
+//     //  ++a; pre increment
+//     //    --a; pre decrement    
+
+//      console.log(a)
+    
+
+
+    // let a = 10;
+    // let b = a++;
+    // // let b = ++a;
+    //  console.log(b)
+
+
+    // let age1 = 10;
+    // let age2 = 15
+    // console.log(`hello my age is ${age1} and my friend age is ${age2}`)
+
+
+    // let a = '10'; //number
+    // let b = 10; // string
+    // console.log(a==b) //only check value
+    // console.log(a===b)
+
+
+
+
+    // logical operator
+    // 1. logical && AND operator
+    // 2. logical || OR operator
+    // 3. logical ! NOT operator
+
+
+
+//   1. && operator
+
+    // console.log(10>5 && 15>6 && 60>50 && 23>20)
+
+    // 2. logical || operator
+
+    //    console.log(10>10 || 12>50 || 88>44)
+
+    // 3. ! operator
+
+        // console.log(!(12>15))
+
+
+//   console.log(10.32>10.33 && !(13>10) || 45>56)
+
+
+//    console.log('A'>'a')
+
---
+++ class3_condition.js
@@ -0,0 +1,48 @@
+// 1. only if
+// 2. if -else
+// 3. else if
+// 4. nested if-else
+// 5. switch statement
+
+
+
+// 1. only if
+
+
+        //  if(condition){
+        //     //code
+        //  }
+
+        //   let a = 10;
+
+        //    if(a>50 || a>4.65){
+        //     console.log("hello")
+        //     console.log("world")
+        //     // let a = 20;
+        //    }
+
+        //    console.log(a)
+
+
+
+        // if(!(10>20)){
+        //     console.log("kullu")
+        // }
+
+
+        // let a = true;
+        // console.log(a)
+
+        // if(false){
+        //     console.log("manali")
+        // }
+
+
+        // let age = 20;
+          
+        // if(age>=18){
+        //     console.log("you can vote")
+        // }
+        // else{
+        //     console.log("you are underage")
+        // }
