9f821524c1237e8f7da88d914ccff19e0c018266
saurabh kumar <saurabhkumar9493@gmail.com>
2024-09-25 12:24:37 +0530

    string

---
+++ class5_string.js
@@ -0,0 +1,148 @@
+
+
+
+// const a = 'raman';
+// const b = 123;
+// const c = true;
+// console.log(typeof(c));
+
+// const a = "ku llu  manali";
+// console.log(a.length)
+
+// const a = "shimla";
+// console.log(a[3])
+
+
+// 1. concat()
+
+    //   const a = " kullu";
+    //   const b = "shimla";
+    // //   console.log(a+"     "+b);
+    // console.log(b.concat(a))
+
+
+
+    // const a = "kullu";
+    // const b = 123;
+    // console.log(typeof(a+b));
+
+    // number+string = string
+    // string+number = string
+    // string+string = string
+    // number+number = number
+
+
+    // 2. indexOf()
+
+    // const a = "kullu";
+    // console.log(a.indexOf('k'))
+
+    // 3. replace,replaceAll
+
+    // const a = "shimlas";
+    // // console.log(a.replace('s','b'))
+    // console.log(a.replaceAll('s','b'))
+
+
+    // 4. search()
+
+    //   const a = "welcome";
+    //   console.log(a.search('l'))
+
+    // 5. toLowercase()
+
+    // const a = "KULLU";
+    // console.log(a.toLowerCase())
+
+    // let user = "KOMAL";
+
+    //    if(user.toLowerCase()=="komal"){
+    //     console.log("login successfully")
+    //    }else{
+    //     console.log("something went error")
+    //    }
+
+    // 6. toUppercase()
+
+    //    const a = "kullu";
+    //    console.log(a.toUpperCase())
+
+
+    // 7. includes()
+
+    // const str = "most welcome";
+
+    //  console.log(str.includes("lcoe"))
+
+        //   const a = "simran kumari";
+
+        //   if(a.includes("kumar") ){
+        //     console.log("login successfully")
+        //   }
+        //   else{
+        //     console.log("something error")
+        //   }
+
+
+        // 8. trim()
+
+        // const a = "        simran    kaur      ";
+        //    console.log(a.trim())
+
+        // 9. startsWith()
+
+            // const a = "most welcome";
+            // console.log(a.startsWith('mo'))
+
+
+            // const name = "aman";
+
+            //   if(name.startsWith('s')){
+            //     console.log("login succefully")
+            //   }else{
+            //     console.log("something error")
+            //   }
+
+            // 10. endsWith()
+
+            // const a = "simran kumari";
+
+            // // console.log(a.endsWith("s"))
+             
+            //    if(a.endsWith('kaur')){
+            //     console.log("you can enter in my cinemahall")
+            //    }else{
+            //     console.log("you cant enter in my cinema hall")
+            //    }
+
+
+            // 11. charAt()
+
+            // const a = "simran";
+            //   console.log(a.charAt(3))
+
+            // 12. split()  ye string ko array me convert karta h 
+              
+            //    const a = "most welcome, hello";
+            // //    console.log(a.split())
+            // // console.log(a.split(''))
+            // console.log(a.split(','))
+
+
+            // 13. repeat()
+             
+                //  const a = "hello";
+                //     console.log(a.repeat(3));
+
+            //  14.substring()
+            
+                //  const a = "welcome";
+
+                // //  console.log(a.substring(1,5))
+                // console.log(a.substring(-4,-1))
+
+                // 15. slice() 
+
+                    // const a = "welcome";
+                    // console.log(a.slice(1,3))
+                    // console.log(a.slice(-4,-1))
