465083c57055970576cd38751a010819d9e6b42b
saurabh kumar <saurabhkumar9493@gmail.com>
2024-10-04 15:15:17 +0530

    highorder

--- class10_highOrder.js
+++ class10_highOrder.js
@@ -37,6 +37,8 @@
 // 2. filter
 // 3. reduce 
 // 4. fill
+// 5. sort
+// 6. foreach 
 
 
 
@@ -44,21 +46,118 @@
 
     //   let arr = [10,20,56,87,23,55];
 
-        // let arr = "hola"
-        let arr = ["komal","kullu","aman"]
+    //     // let arr = "hola"
+    //     let arr = ["komal","kullu","aman"]
 
-        //    arr.map(function momo(a,b,c){
-        //           console.log(a,b,c)
-        //    })
+    //     //    arr.map(function momo(a,b,c){
+    //     //           console.log(a,b,c)
+    //     //    })
    
-        const z  = arr.map((a,b,c)=>{
-            // console.log(a/2)
+        // const z  = arr.map((a,b,c)=>{
+        //     // console.log(a/2)
 
-            return a
-              console.log("shimla")
-              console.log("manali")
+        //     return a+2
+        //       console.log("shimla")
+        //       console.log("manali")
              
-           })
+        //    })
 
-           console.log(z)
-           
+        //    console.log(z)
+
+
+
+//   2. filter
+
+    //   const arr = [10,15,17,20,30]; //5
+
+    //         const value  = arr.filter((a,b,c)=>{
+    //             //    console.log(a,b,c)
+
+    //             //    return a*2
+                      
+    //                  if(a%2==0){
+    //                     return a
+    //                  }
+    //            })
+    //        console.log(value)
+
+
+    //   const z  = arr.map((a,b,c)=>{
+        
+    //           if(a%2==0){
+    //             return a
+    //           }
+    //        })
+
+        //    console.log(z)
+
+
+        // 3. sort function
+
+            // const arr = [44,74,65,5,23];
+            // const arr = ["iphone","samsung","redmi","pixel"]
+
+            //  const value = arr.sort((a,b)=>{
+                       
+            //         //    console.log(a,b)
+            //         // return a-b
+            //         return b-a
+            //    })
+            //    console.log(value)
+
+
+//4.   forEach function
+
+
+        // const arr = [10,41,23,26,80];
+
+        //      const value = arr.forEach((a,b,c)=>{
+        //             //  console.log(a,b,c)
+        //             return a
+        //        })
+
+        //        console.log(value)
+
+
+// 5. reduce
+
+
+        //   const arr = [10,4,23,85,36];
+
+        //       const value  = arr.reduce((x,y,z,a)=>{
+                      
+        //             //   console.log(x,y,z,a)
+
+        //             return x+y
+        //          },40)
+
+        //          console.log(value)
+
+
+
+        //         const a = [] ;
+
+        //               a.push(155500)
+        //               a.push(40000)
+
+        //               console.log(a)
+
+        //            const totalPrice =  a.reduce((x,y)=>{
+                            
+        //                 return x+y
+        //               })
+
+        //               console.log("total price",totalPrice)
+
+
+
+//    setTimeout function
+
+        // setTimeout(()=>{
+        //     console.log("hello")
+        // },5000)
+
+
+        // setInterval(()=>{
+        //     console.log("hello")
+        // },1000)
