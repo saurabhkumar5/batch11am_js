236556e893b9ae7ed7c1517ae2216d82e01ff642
saurabh kumar <saurabhkumar9493@gmail.com>
2024-10-07 15:13:17 +0530

    object

---
+++ class11_object.js
@@ -0,0 +1,116 @@
+// const a = ["aman",12,"kullu"]
+
+//          name = "aman"
+//          age = 12
+//          place = "kullu"
+
+// key-value pair => object
+
+//    [] //array
+//    {} //object
+
+
+                   
+            //    const obj   =  {
+            //             name: "aman",
+            //             age: 12,
+            //             place: "kullu"
+            //         }
+
+                     //dot notation
+                    // console.log(obj.name,obj.age)
+                    // console.log(obj.place)
+
+                    // console.log(obj['name'])
+                    // console.log(obj["age"])
+
+
+        
+
+                //  const obj  = {
+                //         name:"khusi",
+                //         age:12,
+                //         place: {
+                //             country:{
+                //                 firstName:"Bharat",
+                //                 secondName:"India",
+                //                 thirdName:"hindustan",
+                //                 fourthName:"aaryavart"
+                //             },
+                //             state: "HP",
+                //             district: "kullu",
+                //             mohalla: "chai k saamne",
+                //             mob: 123456789
+                //         },
+                //         hobby:["cricket","movies","game"]
+                //     }
+
+                    // console.log(obj)
+                    // console.log(obj.name)
+                    // console.log(obj.place.country)
+                    // console.log(obj.place.district)
+
+                    // console.log(obj.place.country.secondName)
+                    // console.log(obj.hobby[1])
+
+
+                    // let arr = [112,"kullu",true,undefined,[45,68],{name:"simran"}]
+
+                    //   console.log(arr[5].name)
+
+
+                    // console.log({name:"mohali"}.name)
+
+
+            // const obj = {
+            //     name:"sonam",
+            //     age:20,
+            //     hobby:"dancing",
+            // };        
+
+            // obj.place = "mohali";
+            // obj.name = "suman";
+            // delete obj.age
+            // console.log(obj);
+
+
+
+            //   const obj = {
+            //     name:"sonam",
+            //     age:20,
+            //     hobby:"dancing",
+            // }; 
+
+             
+          
+            // for(let a in obj){
+            //     console.log(obj[a])
+            // }
+
+
+
+        
+
+        const arr  =  [
+                {name:"rohan",age:12,hobby:"cricket"},
+                {name:"sohan",age:15,hobby:"dancing"},
+                {name:"suraj",age:18,hobby:"singing"},
+                {name:"komal",age:10,hobby:"cooking"}
+            ]
+
+            // console.log((arr[0].name))
+            // console.log((arr[1].name))
+            // console.log((arr[2].name))
+            // console.log((arr[3].name))
+
+            // for(let a of arr){
+            //     console.log(a.name)
+            // }
+            
+            // using for loop
+
+
+            // arr.map((a)=>{
+            //      console.log(a.name)
+            // })
+            
