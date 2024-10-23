9806b92a805a51cc39bc13bb162fa48428d36c52
saurabh kumar <saurabhkumar9493@gmail.com>
2024-10-23 11:45:53 +0530

    last

---
+++ class16_asyn.js
@@ -0,0 +1,27 @@
+
+ 
+//  setTimeout(()=>{
+//     console.log("kullu")
+// },0)
+// console.log("start")
+// console.log("hello")
+
+// async function momo(a){
+//     console.log("momo",a)
+//   }
+// momo(()=>"world")
+
+// console.log("komal")
+
+
+// momo()
+
+// function momo(){
+//     console.log("hello")
+// }
+// let a = 10;
+// console.log(a);
+
+
+
+const arr = ["kullu","shimla","manali","jammu"]
